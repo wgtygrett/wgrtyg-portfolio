2ff0d697c321d3b1c5d92d9060f295eee1078130
Tygrett <wendy.tygrett@cgi.com>
2025-03-30 20:52:17 -0500

    updated name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wgrtyg-frontend'`, () => {
+  it(`should have as title 'wgrtyg-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app.title).toEqual('wgrtyg-frontend');
+    // expect(app.title).toEqual('wgrtyg-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wgrtyg-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wgrtyg-portfolio');
   });
 });
